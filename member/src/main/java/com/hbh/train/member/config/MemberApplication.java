91cd781807741a03a2159acd829f04d7cc6897c8
hbh <1090989941@qq.com>
2024-11-01 17:47:37 +0800

    mybatis配置

--- member/src/main/java/com/hbh/train/member/config/MemberApplication.java
+++ member/src/main/java/com/hbh/train/member/config/MemberApplication.java
@@ -10,7 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.hbh")
-@MapperScan("com.hbh.train.member.mapper")
+@MapperScan("com.hbh.train.*.mapper")
 public class MemberApplication {
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
