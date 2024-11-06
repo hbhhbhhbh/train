4037418578b06e8f7670361f3bbf6b14da35f6da
hbh <1090989941@qq.com>
2024-11-06 16:25:54 +0800

    登录显示mobile，用store存储member信息

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -2,13 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    member:{}
   },
   getters: {
   },
   mutations: {
+    setMember(state,_member){
+      state.member=_member;
+    }
   },
   actions: {
   },
   modules: {
+
   }
 })
