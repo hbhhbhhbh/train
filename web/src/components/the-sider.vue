7d6326d00bf35282577016f49800cc0ce4fbd80e
hbh <1090989941@qq.com>
2024-11-07 16:58:20 +0800

    前端假数据表格

--- web/src/components/the-sider.vue
+++ web/src/components/the-sider.vue
@@ -15,11 +15,7 @@
           <user-outlined /> &nbsp; 乘车人管理
         </router-link>
       </a-menu-item>
-      <a-menu-item key="/ticket">
-        <router-link to="/ticket">
-          <user-outlined /> &nbsp; 余票查询
-        </router-link>
-      </a-menu-item>
+
     </a-menu>
   </a-layout-sider>
 </template>
--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,15 +1,104 @@
 <template>
-  <h1>乘车人管理</h1>
+  <p>
+  <a-button type="primary" @click="showModal">新增</a-button>
+  </p>
+  <a-table :columns="columns" :dataSource="dataSource" />
+  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
+    <a-form :model="passenger" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }">
+      <a-form-item label="姓名">
+        <a-input v-model:value="passenger.name" />
+      </a-form-item>
+      <a-form-item label="身份证">
+        <a-input v-model:value="passenger.idCard" />
+      </a-form-item>
+      <a-form-item label="旅客类型">
+        <a-select v-model:value="passenger.type">
+          <a-select-option value="1">成人</a-select-option>
+          <a-select-option value="2">儿童</a-select-option>
+          <a-select-option value="3">学生</a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
+
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent,  ref } from 'vue';
+import { notification } from 'ant-design-vue';
+import axios from 'axios';
 
 export default defineComponent({
   setup() {
+    const visible = ref(false);
+    let passenger = ref({
+      id: undefined,
+      memberId: undefined,
+      name: undefined,
+      idCard: undefined,
+      type: undefined,
+      createTime: undefined,
+      updateTime: undefined,
+    });
+    const dataSource= [
+      {
+        key: '1',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+      },
+      {
+        key: '2',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+      },
+    ];
+
+       const columns= [
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+      },
+      {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+      },
+    ];
+
+    const showModal = () => {
+      visible.value = true;
+    };
+    const handleOk = () => {
+      console.log(passenger);
+      axios.post('/member/passenger/save', passenger.value).then((response) => {
+        let data = response.data;
+        console.log(data);
+        if (data.success) {
+          notification.success({ description: '保存成功！' });
+          visible.value = false;
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
+    };
+
     return {
+      visible,
+      showModal,
+      handleOk,
+      passenger,
+      dataSource,
+      columns,
     };
   },
 });
 </script>
-<style>
-</style>
+
+<style></style>
