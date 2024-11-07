75a6965854277f3cf71d4f152c3f9aca0017dde1
hbh <1090989941@qq.com>
2024-11-07 08:53:40 +0800

    登录校验

--- gateway/src/main/java/com/hbh/train/gateway/config/Test1Filter.java
+++ gateway/src/main/java/com/hbh/train/gateway/config/Test1Filter.java
@@ -1,21 +1,21 @@
-package com.hbh.train.gateway.config;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-@Component
-public class Test1Filter implements GlobalFilter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(Test1Filter.class);
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        LOG.info("Test1Filter");
-        return chain.filter(exchange);
-
-    }
-}
+//package com.hbh.train.gateway.config;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//@Component
+//public class Test1Filter implements GlobalFilter {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(Test1Filter.class);
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        LOG.info("Test1Filter");
+//        return chain.filter(exchange);
+//
+//    }
+//}
