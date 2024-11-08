5584f801eedc762bb1ba791b790092f58e37f15e
hbh <1090989941@qq.com>
2024-11-08 13:28:35 +0800

    获取表名和实体名

---
+++ generator/src/main/java/com/hbh/train/generator/gen/ServerGenerator.java
@@ -0,0 +1,45 @@
+package com.hbh.train.generator.gen;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerGenerator {
+    static boolean readOnly = true;
+    static String vuePath = "admin/src/views/main/";
+    static String serverPath = "[module]/src/main/java/com/hbh/train/[module]/";
+    static String pomPath = "generator/pom.xml";
+    static String module = "";
+    static {
+        new File(serverPath).mkdirs();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 获取mybatis-generator
+        String generatorPath = getGeneratorPath();
+        // 读取table节点
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+    }
+    private static String getGeneratorPath() throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+        return node.getText();
+    }
+
+}
