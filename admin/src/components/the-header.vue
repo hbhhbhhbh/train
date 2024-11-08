20311fc25cb3fe7f7a3b31e59ad3eb1c9334d736
hbh <1090989941@qq.com>
2024-11-08 15:32:41 +0800

    去掉登陆有关的东西

--- admin/src/components/the-header.vue
+++ admin/src/components/the-header.vue
@@ -1,15 +1,10 @@
 <template>
   <a-layout-header class="header">
     <div class="logo">
-      <router-link to="/welcome" style="color: white; font-size: 18px">
-        hbh12306
-      </router-link>
+
     </div>
     <div style="float: right; color: white;">
-      您好：{{member.mobile}} &nbsp;&nbsp;
-      <router-link to="/login" style="color: white;">
-        退出登录
-      </router-link>
+      管理控制台
     </div>
     <a-menu
         v-model:selectedKeys="selectedKeys"
@@ -22,9 +17,9 @@
           <coffee-outlined /> &nbsp; 欢迎
         </router-link>
       </a-menu-item>
-      <a-menu-item key="/passenger">
-        <router-link to="/passenger">
-          <user-outlined /> &nbsp; 乘车人管理
+      <a-menu-item key="/about">
+        <router-link to="/about">
+          <user-outlined />  $nbsp;关于
         </router-link>
       </a-menu-item>
 
@@ -35,13 +30,13 @@
 
 <script>
 import {defineComponent, ref, watch} from 'vue';
-import store from "@/store";
+
 import router from '@/router'
 
 export default defineComponent({
   name: "the-header-view",
   setup() {
-    let member = store.state.member;
+
     const selectedKeys = ref([]);
     watch(
         () => router.currentRoute.value.fullPath,
@@ -50,7 +45,7 @@ export default defineComponent({
         }
     );
     return {
-      member,
+
       selectedKeys
     };
   },
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,13 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import store from "@/store";
-import { notification } from "ant-design-vue";
-
 const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/the-login.vue')
-  },
   {
     path: '/',
     name: 'main',
@@ -21,8 +13,8 @@ const routes = [
         component: () => import(/* webpackChunkName: "about"*/'../views/main/welcome.vue')
       },
       {
-        path: 'passenger',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/passenger.vue')
+        path: 'about',
+        component: () => import(/* webpackChunkName: "about"*/'../views/main/about.vue')
       }
     ]
   },
@@ -36,25 +28,4 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-// 路由登录拦截
-router.beforeEach((to, from, next) => {
-  // 要不要对meta.loginRequire属性做监控拦截
-  if (to.matched.some(function (item) {
-    console.log(item, "是否需要登录校验：", item.meta.loginRequire );
-    return item.meta.loginRequire
-  })) {
-    const _member = store.state.member;
-    console.log("页面登录校验开始：", _member);
-    if (!_member.token) {
-      console.log("用户未登录或登录超时！");
-      notification.error({ description: "未登录或登录超时" });
-      next('/login');
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
-});
-
 export default router
