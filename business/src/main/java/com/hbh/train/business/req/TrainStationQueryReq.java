bde31961f7f0c9d3a1c4406cefc42a7387dca94f
hbh <1090989941@qq.com>
2024-11-11 14:32:06 +0800

    增加车次查询条件

--- business/src/main/java/com/hbh/train/business/req/TrainStationQueryReq.java
+++ business/src/main/java/com/hbh/train/business/req/TrainStationQueryReq.java
@@ -3,10 +3,21 @@ package com.hbh.train.business.req;
 import com.hbh.train.common.req.PageReq;
 
 public class TrainStationQueryReq extends PageReq {
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
         return "TrainStationQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
     }
+
+    private String trainCode;
+
 }
