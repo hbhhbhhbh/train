f33359644b9bb2f235180ad97cf02abf51d1fe4e
hbh <1090989941@qq.com>
2024-11-12 10:28:45 +0800

    更改路由，进行路由归类

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,53 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
-const routes = [
-  {
-    path: '/',
-    name: 'main',
-    component: () => import(/* webpackChunkName: "about" */ '../views/main.vue'),
-    meta: {
-      loginRequire: true
-    },
-    children: [
-      {
-        path: 'welcome',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/welcome.vue')
-      },
-      {
-        path: 'about',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/about.vue')
-      },
-      {
-        path: 'station',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/business/station.vue')
-      },
-      {
-        path: 'train-station',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/business/train-station.vue')
-      },
-      {
-        path: 'train',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/business/train.vue')
-      },
-      {
-        path: 'train-carriage',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/business/train-carriage.vue')
-      },
-      {
-        path: 'train-seat',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/business/train-seat.vue')
-      }
-      ,
-      {
-        path: 'batch/job',
-        component: () => import(/* webpackChunkName: "about"*/'../views/main/batch/job.vue')
-      }
-    ]
-  },
-  {
-    path: '',
-    redirect: '/welcome'
-  }
-]
+
+const routes = [{
+  path: '/',
+  component: () => import('../views/main.vue'),
+  children: [{
+    path: 'welcome',
+    component: () => import('../views/main/welcome.vue'),
+  }, {
+    path: 'about',
+    component: () => import('../views/main/about.vue'),
+  }, {
+    path: 'base/',
+    children: [{
+      path: 'station',
+      component: () => import('../views/main/business/station.vue'),
+    }, {
+      path: 'train',
+      component: () => import('../views/main/business/train.vue'),
+    }, {
+      path: 'train-station',
+      component: () => import('../views/main/business/train-station.vue'),
+    }, {
+      path: 'train-carriage',
+      component: () => import('../views/main/business/train-carriage.vue'),
+    }, {
+      path: 'train-seat',
+      component: () => import('../views/main/business/train-seat.vue'),
+    }]
+  }, {
+    path: 'batch/',
+    children: [{
+      path: 'job',
+      component: () => import('../views/main/batch/job.vue')
+    }]
+  }]
+}, {
+  path: '',
+  redirect: '/welcome'
+}];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
