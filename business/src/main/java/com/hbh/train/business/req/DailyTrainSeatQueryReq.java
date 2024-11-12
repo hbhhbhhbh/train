d7db3b95a092a3e6ddee4968ff2b53599e13518a
hbh <1090989941@qq.com>
2024-11-12 14:26:59 +0800

    快速生成车位管理

---
+++ business/src/main/java/com/hbh/train/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package com.hbh.train.business.req;
+
+import com.hbh.train.common.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
