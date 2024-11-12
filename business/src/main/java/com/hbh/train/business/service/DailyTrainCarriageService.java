f7ea4751348a76e46ed1788cbb232289943311c6
hbh <1090989941@qq.com>
2024-11-12 14:14:02 +0800

    快速生成车厢管理

--- business/src/main/java/com/hbh/train/business/service/DailyTrainCarriageService.java
+++ business/src/main/java/com/hbh/train/business/service/DailyTrainCarriageService.java
@@ -5,6 +5,7 @@ import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.hbh.train.business.enums.SeatColEnum;
 import com.hbh.train.common.resp.PageResp;
 import com.hbh.train.common.util.SnowUtil;
 import com.hbh.train.business.domain.DailyTrainCarriage;
@@ -30,6 +31,10 @@ public class DailyTrainCarriageService {
 
     public void save(DailyTrainCarriageSaveReq req) {
         DateTime now = DateTime.now();
+        List<SeatColEnum>seatColEnums=SeatColEnum.getColsByType(req.getSeatType());
+        req.setColCount(seatColEnums.size());
+        req.setSeatCount(req.getColCount()*req.getRowCount());
+
         DailyTrainCarriage dailyTrainCarriage = BeanUtil.copyProperties(req, DailyTrainCarriage.class);
         if (ObjectUtil.isNull(dailyTrainCarriage.getId())) {
             dailyTrainCarriage.setId(SnowUtil.getSnowflakeNextId());
