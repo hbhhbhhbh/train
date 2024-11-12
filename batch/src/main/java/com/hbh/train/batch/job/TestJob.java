603449ef1ee53b96ffec1e12a5afa74ce69ca55c
hbh <1090989941@qq.com>
2024-11-12 09:13:18 +0800

    禁止任务并发执行

--- batch/src/main/java/com/hbh/train/batch/job/TestJob.java
+++ batch/src/main/java/com/hbh/train/batch/job/TestJob.java
@@ -1,12 +1,19 @@
 package com.hbh.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("testJob");
+        System.out.println("testJob-st");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("testJob-ed");
     }
 }
