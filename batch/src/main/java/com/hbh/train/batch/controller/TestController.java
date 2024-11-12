3963c0750036b2e37f767d32fb8ad64285c2d7b3
hbh <1090989941@qq.com>
2024-11-12 15:27:01 +0800

    集成openfeign，实现模块调用

--- batch/src/main/java/com/hbh/train/batch/controller/TestController.java
+++ batch/src/main/java/com/hbh/train/batch/controller/TestController.java
@@ -1,12 +1,22 @@
 package com.hbh.train.batch.controller;
 
+import com.hbh.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    BusinessFeign businessFeign;
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        String businessHello= businessFeign.hello();
+        LOG.info(businessHello);
+        return businessHello;
     }
 }
