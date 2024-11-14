68561a42e215374a4ba832c894fad79ed1cf8cdb
hbh <1090989941@qq.com>
2024-11-14 15:48:14 +0800

    保存确认订单表，状态初始

--- business/src/main/java/com/hbh/train/business/controller/admin/ConfirmOrderAdminController.java
+++ business/src/main/java/com/hbh/train/business/controller/admin/ConfirmOrderAdminController.java
@@ -1,10 +1,9 @@
 package com.hbh.train.business.controller.admin;
 
-import com.hbh.train.common.context.LoginMemberContext;
 import com.hbh.train.common.resp.CommonResp;
 import com.hbh.train.common.resp.PageResp;
 import com.hbh.train.business.req.ConfirmOrderQueryReq;
-import com.hbh.train.business.req.ConfirmOrderSaveReq;
+import com.hbh.train.business.req.ConfirmOrderDoReq;
 import com.hbh.train.business.resp.ConfirmOrderQueryResp;
 import com.hbh.train.business.service.ConfirmOrderService;
 import jakarta.annotation.Resource;
@@ -19,7 +18,7 @@ public class ConfirmOrderAdminController {
     private ConfirmOrderService confirmOrderService;
 
     @PostMapping("/save")
-    public CommonResp<Object> save(@Valid @RequestBody ConfirmOrderSaveReq req) {
+    public CommonResp<Object> save(@Valid @RequestBody ConfirmOrderDoReq req) {
         confirmOrderService.save(req);
         return new CommonResp<>();
     }
--- business/src/main/java/com/hbh/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/hbh/train/business/service/ConfirmOrderService.java
@@ -3,20 +3,25 @@ package com.hbh.train.business.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.hbh.train.common.resp.PageResp;
-import com.hbh.train.common.util.SnowUtil;
 import com.hbh.train.business.domain.ConfirmOrder;
 import com.hbh.train.business.domain.ConfirmOrderExample;
+import com.hbh.train.business.enums.ConfirmOrderStatusEnum;
 import com.hbh.train.business.mapper.ConfirmOrderMapper;
+import com.hbh.train.business.req.ConfirmOrderDoReq;
 import com.hbh.train.business.req.ConfirmOrderQueryReq;
-import com.hbh.train.business.req.ConfirmOrderSaveReq;
 import com.hbh.train.business.resp.ConfirmOrderQueryResp;
+import com.hbh.train.common.context.LoginMemberContext;
+import com.hbh.train.common.resp.PageResp;
+import com.hbh.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -28,7 +33,7 @@ public class ConfirmOrderService {
     @Resource
     private ConfirmOrderMapper confirmOrderMapper;
 
-    public void save(ConfirmOrderSaveReq req) {
+    public void save(ConfirmOrderDoReq req) {
         DateTime now = DateTime.now();
         ConfirmOrder confirmOrder = BeanUtil.copyProperties(req, ConfirmOrder.class);
         if (ObjectUtil.isNull(confirmOrder.getId())) {
@@ -66,5 +71,40 @@ public class ConfirmOrderService {
 
     public void delete(Long id) {
         confirmOrderMapper.deleteByPrimaryKey(id);
+    }
+    public void doConfirm(@Valid @RequestBody  ConfirmOrderDoReq req) {
+        // 省略业务数据校验，如：车次是否存在，余票是否存在，车次是否在有效期内，tickets条数>0，同乘客同车次是否已买过
+        ConfirmOrder confirmOrder = new ConfirmOrder();
+        DateTime now = DateTime.now();
+        confirmOrder.setId(SnowUtil.getSnowflakeNextId());
+        confirmOrder.setMemberId(LoginMemberContext.getId());
+        confirmOrder.setDate(req.getDate());
+        confirmOrder.setTrainCode(req.getTrainCode());
+        confirmOrder.setStart(req.getStart());
+        confirmOrder.setEnd(req.getEnd());
+        confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
+        confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
+        confirmOrder.setCreateTime(now);
+        confirmOrder.setUpdateTime(now);
+        confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+
+        // 保存确认订单表，状态初始
+        confirmOrderMapper.insert(confirmOrder);
+                // 查出余票记录，需要得到真实的库存
+
+                // 扣减余票数量，并判断余票是否足够
+
+                // 选座
+
+                // 一个车箱一个车箱的获取座位数据
+
+                // 挑选符合条件的座位，如果这个车箱不满足，则进入下个车箱（多个选座应该在同一个车厢）
+
+                // 选中座位后事务处理：
+
+                // 座位表修改售卖情况sell；
+                // 余票详情表修改余票；
+                // 为会员增加购票记录
+                // 更新确认订单为成功
     }
 }
