a85b0cc4ba4d62ec1c84fa60a8e047a701447603
hbh <1090989941@qq.com>
2024-11-18 15:27:45 +0800

    集成nacos注册中心，实现按应用名进行路由转发

--- member/src/main/java/com/hbh/train/member/controller/TestController.java
+++ member/src/main/java/com/hbh/train/member/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.hbh.train.member.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
@@ -9,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @Value("${test.nacos}")
     private String nacos;
+    @Autowired
+    Environment environment;
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!"+nacos;
+        return "Hello World!"+nacos+ environment.getProperty("local.server.port");
     }
 }
