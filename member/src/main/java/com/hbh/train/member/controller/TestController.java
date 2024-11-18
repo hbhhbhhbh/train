7c6aef1174684c4a40672dc5e53b2c045232b494
hbh <1090989941@qq.com>
2024-11-18 14:27:37 +0800

    集成nacos配置中心，实现动态刷新

--- member/src/main/java/com/hbh/train/member/controller/TestController.java
+++ member/src/main/java/com/hbh/train/member/controller/TestController.java
@@ -1,9 +1,10 @@
 package com.hbh.train.member.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope
 @RestController
 public class TestController {
     @Value("${test.nacos}")
