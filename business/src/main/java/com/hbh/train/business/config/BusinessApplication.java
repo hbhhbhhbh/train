c00eb1ff839f2014949c1e90dc8156c41477003d
hbh <1090989941@qq.com>
2024-11-28 11:12:40 +0800

    redis集成

--- business/src/main/java/com/hbh/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/hbh/train/business/config/BusinessApplication.java
@@ -5,10 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-
+@EnableCaching
 @SpringBootApplication
 @ComponentScan("com.hbh")
 @MapperScan("com.hbh.train.*.mapper")
--- business/src/main/java/com/hbh/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/hbh/train/business/req/DailyTrainTicketQueryReq.java
@@ -4,6 +4,7 @@ import com.hbh.train.common.req.PageReq;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class DailyTrainTicketQueryReq extends PageReq {
 
@@ -55,4 +56,19 @@ public class DailyTrainTicketQueryReq extends PageReq {
     }
 
     private String trainCode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DailyTrainTicketQueryReq that = (DailyTrainTicketQueryReq) o;
+        return Objects.equals(date, that.date) && Objects.equals(start, that.start)
+                && Objects.equals(end, that.end) && Objects.equals(trainCode, that.trainCode)
+                && Objects.equals(((DailyTrainTicketQueryReq) o).getPage(), that.getPage()) && Objects.equals(((DailyTrainTicketQueryReq) o).getSize(), that.getSize());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, start, end, trainCode,getPage(),getSize());
+    }
 }
